55d39abe117db7cb7da2a19ed0bc0d2715c8591c
Gautam Sharma <gautamgaur2004@gamil.com>
2024-10-04 23:39:47 +0530

    java practies

---
+++ output ways.java
@@ -0,0 +1,27 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        // way to output in java
+        // print number
+        System.out.println(100);
+        System.out.println(0);
+        System.out.println(1010);
+        
+        // sum of two number
+        System.out.println(10 + 10);
+        System.out.println(10 * 10);
+        System.out.println(10 - 10);
+        System.out.println(10 / 10);
+        System.out.println(10 % 10);
+        System.out.println(10 ^ 10);
+        System.out.println(10 ^ 11);
+        
+        // string with integer checking
+        System.out.println("10" + 10);  // 1010
+        System.out.println("10" + "10");  // 1010
+        
+    }
+}
