4d18402a88ebb4ebeeabdf3ed8e23ac9b2856e93
Gautam Sharma <gautamgaur2004@gamil.com>
2024-10-05 00:08:03 +0530

    new file

---
+++ Casting.java
@@ -0,0 +1,21 @@
+class HelloWorld {
+    public static void main(String[] args){
+
+        // variable with number sum 
+        int firstNumber = 10;
+        int secondNumber = 100;
+        int add = firstNumber + secondNumber;
+
+        System.out.println("Tow number of the sum : "+ add );
+
+
+        // casting 
+        // casting in java occurs automatic
+        double decimal = 100;
+        System.out.println("casting number print: " + decimal);
+
+        // Narrowing Casting is also happens
+        double deci = (double) 102;
+        System.out.println("casting number print again: " + deci);
+    }
+}
