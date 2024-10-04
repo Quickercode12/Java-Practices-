8cd137cc38903659b60296a7687fcb6d5df968d7
Gautam Sharma <gautamgaur2004@gamil.com>
2024-10-04 23:27:42 +0530

    Java Practies

---
+++ Hello World.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+    public static void main(String[],args){
+
+        // print the for rule
+        System.out.println("Hello world");
+
+        System.out.println("New Developer");
+
+        System.out.println("My love is just stay in coding");
+
+
+    }
+}
