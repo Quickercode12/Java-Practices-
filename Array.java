d5573e191b93c9def438a02c631bc2b21c54afd8
Gautam Sharma <gautamgaur2004@gamil.com>
2024-10-05 20:11:10 +0530

    array concept

---
+++ Array.java
@@ -0,0 +1,17 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        // array in java
+        // if we are using array then we need to defined the variable such as String[](if we have data string type of), int[](if we have data integer type of), etx
+        
+        String[] nameCollection = {"Shiv", "Ganesh","Hanuman","Krishna"};
+        
+        // array take the data by the index or indexing will start from 0 always
+        System.out.println("array of the second data: " + nameCollection[2]);
+        
+        // same we can apply on the integer for
+    }
+}
